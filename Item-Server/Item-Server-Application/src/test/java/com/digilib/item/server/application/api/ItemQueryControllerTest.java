5b9dd659ad66dc48370cc562ad9a110631859772
Saltator <saltatorv@gmail.com>
2023-12-24 21:24:38 +0100

    refactor: Item-Server-Application: ItemQueryControllerTest rename private methods (#5)

--- Item-Server/Item-Server-Application/src/test/java/com/digilib/item/server/application/api/ItemQueryControllerTest.java
+++ Item-Server/Item-Server-Application/src/test/java/com/digilib/item/server/application/api/ItemQueryControllerTest.java
@@ -1,15 +1,17 @@
 package com.digilib.item.server.application.api;
 
+import com.digilib.item.server.service.dto.response.ItemResponse;
 import com.digilib.item.server.service.port.input.ItemQueryService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import java.util.UUID;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 public class ItemQueryControllerTest {
 
     @Mock
@@ -22,8 +24,8 @@ public class ItemQueryControllerTest {
     @Test
     public void shouldReturnSameISBN() {
         //given
-        String ISBN = createRandomISBN();
-        doReturn(ISBN)
+        String ISBN = createISBN();
+        doReturn(createItemResponse(ISBN))
                 .when(itemQueryService)
                 .findItem(ISBN);
 
@@ -35,7 +37,11 @@ public class ItemQueryControllerTest {
     }
 
 
-    private String createRandomISBN() {
-        return UUID.randomUUID().toString();
+    private String createISBN() {
+        return "978-83-644-7691-4";
+    }
+
+    private ItemResponse createItemResponse(String ISBN) {
+        return ItemResponse.create(ISBN);
     }
 }
