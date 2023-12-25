65045343cd62145001cfc4c081b6afb447ab98c7
Saltator <saltatorv@gmail.com>
2023-12-25 18:18:07 +0100

    feat: Item-Server-Service: implement an ItemQueryService (#10)

---
+++ Item-Server/Item-Server-Service/src/test/java/com/digilib/item/server/service/ItemQueryServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.digilib.item.server.service;
+
+import com.digilib.item.server.service.dto.response.ItemSummaryResponse;
+import com.digilib.item.server.service.port.input.ItemQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItemQueryServiceImplTest {
+
+    private ItemQueryService itemQueryService;
+
+    @BeforeEach
+    public void setup(){
+        itemQueryService = new ItemQueryServiceImpl();
+    }
+
+    @Test
+    public void shouldReturnItemResponseWithSameISBN() {
+        //given
+        String ISBN = createISBN();
+
+        //when
+        var result = itemQueryService.findItem(ISBN);
+
+        //then
+        assertEquals(ISBN, result.getISBN());
+    }
+
+    @Test
+    public void shouldReturnListOf3SummaryItems() {
+        //when
+        var result = itemQueryService.fetchItemsSummary();
+
+        //then
+        assertEquals(3, result.size());
+    }
+
+    public String createISBN() {
+        return "0-061-96436-0";
+    }
+
+}
