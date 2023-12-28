b2dc993c2a4fe12981a2765a291c331245290d68
Saltator <saltatorv@gmail.com>
2023-12-28 18:04:58 +0100

    feat: Item-Server: particularize create item command (#12)

--- Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/dto/command/CreateItemCommand.java
+++ Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/dto/command/CreateItemCommand.java
@@ -1,13 +1,39 @@
 package com.digilib.item.server.service.dto.command;
 
+import java.util.Date;
+
 public class CreateItemCommand {
     private String ISBN;
+    private String title;
+    private String author;
+    private String publisher;
+    private Date releaseDate;
 
-    public CreateItemCommand(String ISBN) {
+    public CreateItemCommand(String ISBN, String title, String author, String publisher, Date releaseDate) {
         this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.releaseDate = releaseDate;
     }
 
     public String getISBN() {
         return ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
     }
 }
