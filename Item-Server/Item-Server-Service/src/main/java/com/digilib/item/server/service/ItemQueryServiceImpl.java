b7ed72a207c1ac86d08fced02f1db2302024e617
Saltator <saltatorv@gmail.com>
2023-12-29 16:20:34 +0100

    feat: Item-Server-Service: update ItemQueryServiceImpl::fetchItemsSummary to use new ItemSummaryResponse (#13)

--- Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemQueryServiceImpl.java
+++ Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemQueryServiceImpl.java
@@ -5,6 +5,7 @@ import com.digilib.item.server.service.dto.response.ItemSummaryResponse;
 import com.digilib.item.server.service.port.input.ItemQueryService;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.List;
 
 @Service
@@ -16,10 +17,13 @@ public class ItemQueryServiceImpl implements ItemQueryService {
 
     @Override
     public List<ItemSummaryResponse> fetchItemsSummary() {
+        byte[] avatarBytes = Base64.getDecoder().decode("/9j/4AAQAAANAAAABAAEAA");
+
+
         return List.of(
-                ItemSummaryResponse.create("Title-1"),
-                ItemSummaryResponse.create("Title-2"),
-                ItemSummaryResponse.create("Title-3")
+                ItemSummaryResponse.create("Title-1", "Author-1", avatarBytes),
+                ItemSummaryResponse.create("Title-2", "Author-2", avatarBytes),
+                ItemSummaryResponse.create("Title-3", "Author-3", avatarBytes)
         );
     }
 }
