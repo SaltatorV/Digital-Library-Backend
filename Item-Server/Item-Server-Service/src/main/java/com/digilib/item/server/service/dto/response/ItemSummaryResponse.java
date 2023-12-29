ef9dbb665c324a0dcd3b600904b8f0a9513d0db2
Saltator <saltatorv@gmail.com>
2023-12-29 16:20:00 +0100

    feat: Item-Server-Service: particularize ItemSummaryResponse (#13)

--- Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/dto/response/ItemSummaryResponse.java
+++ Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/dto/response/ItemSummaryResponse.java
@@ -2,16 +2,28 @@ package com.digilib.item.server.service.dto.response;
 
 public class ItemSummaryResponse {
     private final String title;
+    private final String author;
+    private final byte[] avatar;
 
-    private ItemSummaryResponse(String title) {
+    private ItemSummaryResponse(String title, String author, byte[] avatar) {
         this.title = title;
+        this.author = author;
+        this.avatar = avatar;
     }
 
-    public static ItemSummaryResponse create(String title) {
-        return new ItemSummaryResponse(title);
+    public static ItemSummaryResponse create(String title, String author, byte[] avatar) {
+        return new ItemSummaryResponse(title, author, avatar);
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
     }
 }
