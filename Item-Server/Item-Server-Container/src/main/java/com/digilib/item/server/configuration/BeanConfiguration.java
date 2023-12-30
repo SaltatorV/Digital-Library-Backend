008a6b9cba06e1361cac64c174c285b3c282c9a2
Saltator <saltatorv@gmail.com>
2023-12-30 19:35:05 +0100

    feat: Item-Server-Container temp implementation of ItemRepository (#17)

--- Item-Server/Item-Server-Container/src/main/java/com/digilib/item/server/configuration/BeanConfiguration.java
+++ Item-Server/Item-Server-Container/src/main/java/com/digilib/item/server/configuration/BeanConfiguration.java
@@ -1,8 +1,27 @@
 package com.digilib.item.server.configuration;
 
+import com.digilib.item.server.service.port.output.ItemRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class BeanConfiguration {
 
+    @Bean
+    ItemRepository itemRepository() {
+        return new ItemRepository() {
+            List<String> ISBNs = List.of("1111", "2222", "3333", "4444");
+            @Override
+            public Optional<String> findByISBN(String ISBN) {
+                return ISBNs
+                        .stream()
+                        .filter(x->x.equals(ISBN))
+                        .findFirst();
+            }
+        };
+    }
 }
