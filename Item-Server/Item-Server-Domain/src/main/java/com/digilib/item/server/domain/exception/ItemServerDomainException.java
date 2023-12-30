04f58de7a10aeec0d71baff6c563a08672d1d080
Saltator <saltatorv@gmail.com>
2023-12-30 19:58:21 +0100

    feat: Item-Server-Domain - create ItemServerDomainException(#18)

---
+++ Item-Server/Item-Server-Domain/src/main/java/com/digilib/item/server/domain/exception/ItemServerDomainException.java
@@ -0,0 +1,9 @@
+package com.digilib.item.server.domain.exception;
+
+import com.digilib.common.domain.exception.DomainException;
+
+public class ItemServerDomainException extends DomainException {
+    public ItemServerDomainException(String message) {
+        super(message);
+    }
+}
