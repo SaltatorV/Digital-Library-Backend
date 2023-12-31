d9529f413daffb4ee191fd730c73e73abf67fde0
Saltator <saltatorv@gmail.com>
2023-12-31 20:43:01 +0100

    feat: Item-Server-Domain - update Item with isbn property (#18)

--- Item-Server/Item-Server-Domain/src/main/java/com/digilib/item/server/domain/Item.java
+++ Item-Server/Item-Server-Domain/src/main/java/com/digilib/item/server/domain/Item.java
@@ -4,19 +4,19 @@ import com.digilib.item.server.domain.vo.ItemId;
 import com.digilib.item.server.domain.vo.ItemSnapshot;
 
 class Item {
-    private ItemId id;
+    private final ItemId id;
+    private final String isbn;
 
-    public Item() {
-        initialize();
+    private Item(ItemId id, String isbn) {
+        this.id = id;
+        this.isbn = isbn;
+    }
+
+    public static Item initializeItem(ItemSnapshot snapshot) {
+        return new Item(ItemId.generate(), snapshot.getIsbn());
     }
 
     public ItemSnapshot createSnapshot() {
-        return new ItemSnapshot(id.getId());
+        return new ItemSnapshot(id.getId(), isbn);
     }
-
-    private void initialize() {
-        this.id = ItemId.generate();
-    }
-
-
 }
