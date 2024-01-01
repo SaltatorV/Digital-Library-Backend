1348bdc516541947a4c299f156467f84d92464f7
Saltator <saltatorv@gmail.com>
2024-01-01 18:50:50 +0100

    feat: Item-Server-Service - update ItemCommandFacadeImpl with throwing errors (#22)

--- Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemCommandFacadeImpl.java
+++ Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemCommandFacadeImpl.java
@@ -1,9 +1,12 @@
 package com.digilib.item.server.service;
 
 import com.digilib.item.server.domain.ItemDomainFacade;
+import com.digilib.item.server.domain.exception.ItemAlreadyExistsException;
+import com.digilib.item.server.domain.exception.ItemNotFoundException;
 import com.digilib.item.server.domain.vo.ItemSnapshot;
 import com.digilib.item.server.service.dto.command.CreateItemCommand;
 import com.digilib.item.server.service.dto.command.CreateItemDetailsCommand;
+import com.digilib.item.server.service.dto.command.UpdateItemCommand;
 import com.digilib.item.server.service.dto.response.MessageResponse;
 import com.digilib.item.server.service.port.input.ItemCommandFacade;
 import com.digilib.item.server.service.port.output.ItemRepository;
@@ -22,24 +25,54 @@ class ItemCommandFacadeImpl implements ItemCommandFacade {
 
     @Override
     public MessageResponse createItem(CreateItemCommand command) {
-        ItemSnapshot snapshot = new ItemSnapshot(command.getISBN());
+
+        if(isbnExists(command.getISBN())) {
+            throw new ItemAlreadyExistsException();
+        }
+
+        ItemSnapshot snapshot = new ItemSnapshot(command.getISBN(), command.getAuthor(),
+                command.getTitle(), command.getPublisher());
+
         ItemSnapshot initializedSnapshot = itemDomainFacade.createItem(snapshot);
         repository.save(initializedSnapshot);
         return MessageResponse.create("Item successfully created!");
     }
 
     @Override
-    public MessageResponse deleteItem(String isbn) {
+    public MessageResponse deleteItem(String ISBN) {
+        if(isbnNotExists(ISBN)) {
+            throw new ItemNotFoundException();
+        }
+
+        repository.delete(ISBN);
         return MessageResponse.create("Item successfully deleted!");
     }
 
     @Override
-    public MessageResponse updateItem(String isbn) {
+    public MessageResponse updateItem(String ISBN, UpdateItemCommand command) {
+
+        if(isbnNotExists(ISBN)) {
+            throw new ItemNotFoundException();
+        }
+
+        ItemSnapshot snapshot = new ItemSnapshot(ISBN,
+                command.getAuthor(), command.getTitle(), command.getPublisher());
+
+        repository.update(snapshot);
+
         return MessageResponse.create("Item successfully updated!");
     }
 
     @Override
     public MessageResponse createItemDetails(String isbn, CreateItemDetailsCommand command) {
         return MessageResponse.create("Item details successfully bounded!");
+    }
+
+    private boolean isbnExists(String ISBN) {
+        return repository.existsByISBN(ISBN);
+    }
+
+    private boolean isbnNotExists(String ISBN) {
+        return !isbnExists(ISBN);
     }
 }
