1b01779015755ab94916a44c99d1460736fc9287
Saltator <saltatorv@gmail.com>
2024-01-01 17:45:58 +0100

    feat: Item-Server-Service - repository save in ItemCommandFacadeImpl::createItem (#22)

--- Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemCommandFacadeImpl.java
+++ Item-Server/Item-Server-Service/src/main/java/com/digilib/item/server/service/ItemCommandFacadeImpl.java
@@ -6,21 +6,25 @@ import com.digilib.item.server.service.dto.command.CreateItemCommand;
 import com.digilib.item.server.service.dto.command.CreateItemDetailsCommand;
 import com.digilib.item.server.service.dto.response.MessageResponse;
 import com.digilib.item.server.service.port.input.ItemCommandFacade;
+import com.digilib.item.server.service.port.output.ItemRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 class ItemCommandFacadeImpl implements ItemCommandFacade {
 
     private final ItemDomainFacade itemDomainFacade;
+    private final ItemRepository repository;
 
-    public ItemCommandFacadeImpl(ItemDomainFacade itemDomainFacade) {
+    public ItemCommandFacadeImpl(ItemDomainFacade itemDomainFacade, ItemRepository repository) {
         this.itemDomainFacade = itemDomainFacade;
+        this.repository = repository;
     }
 
     @Override
     public MessageResponse createItem(CreateItemCommand command) {
         ItemSnapshot snapshot = new ItemSnapshot(command.getISBN());
         ItemSnapshot initializedSnapshot = itemDomainFacade.createItem(snapshot);
+        repository.save(initializedSnapshot);
         return MessageResponse.create("Item successfully created!");
     }
 
