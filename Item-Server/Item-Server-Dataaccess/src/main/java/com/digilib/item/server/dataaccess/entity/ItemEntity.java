b1d32e3db7b41c6e98f3ea3db428e98d6c00643e
Saltator <saltatorv@gmail.com>
2024-01-11 19:22:51 +0100

    feat: create item entity(#23)

---
+++ Item-Server/Item-Server-Dataaccess/src/main/java/com/digilib/item/server/dataaccess/entity/ItemEntity.java
@@ -0,0 +1,25 @@
+package com.digilib.item.server.dataaccess.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemEntity {
+
+    @Id
+    private String itemId;
+    private String isbn;
+    private String genre;
+    private String title;
+    private String author;
+    private String publisher;
+    private Date releaseDate;
+}
