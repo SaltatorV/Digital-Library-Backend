90b07de2cefe6611d16f80e706d778adf5950b71
Saltator <saltatorv@gmail.com>
2024-03-18 22:25:14 +0100

    #34 Item-Server-Application - refactor ItemQueryController tests

--- Item-Server/Item-Server-Application/src/test/java/com/digilib/item/server/application/api/ItemQueryControllerTest.java
+++ Item-Server/Item-Server-Application/src/test/java/com/digilib/item/server/application/api/ItemQueryControllerTest.java
@@ -27,38 +27,59 @@ public class ItemQueryControllerTest {
     public void shouldReturnSameISBN() {
         //given
         var ISBN = "978-0547928227";
-        doReturn(createItemResponse(ISBN))
-                .when(itemQueryFacade)
-                .findItem(ISBN);
+
+        returnItemResponseWhenGetISBN(ISBN);
 
         //when
-        var response = itemQueryController.findItem(ISBN);
+        var response = findItem(ISBN);
 
         //then
-        assertEquals(ISBN, response.getISBN());
+        assertISBNEquals(ISBN, response);
     }
 
     @Test
     public void shouldReturnListOfSummaryItems() {
         //given
+        returnSummaryItems();
+
+        //when
+        var response = findItemsSummary();
+
+        //then
+        assertItemsSummarySizeIs(3, response);
+    }
+
+    private void returnItemResponseWhenGetISBN(String ISBN) {
+        doReturn(createItemResponse(ISBN))
+                .when(itemQueryFacade)
+                .findItem(ISBN);
+    }
+
+    private void returnSummaryItems() {
         List<ItemSummaryResponse> items = createSummaryWith3Items();
         doReturn(items)
                 .when(itemQueryFacade)
                 .fetchItemsSummary();
-
-        //when
-        var response = itemQueryController.findItemsSummary();
-
-        //then
-        assertEquals(items, response);
     }
 
-    private String createISBN() {
-        return "978-83-644-7691-4";
+    private ItemResponse findItem(String ISBN) {
+        return itemQueryController.findItem(ISBN);
+    }
+
+    private List<ItemSummaryResponse> findItemsSummary() {
+        return itemQueryController.findItemsSummary();
     }
 
     private ItemResponse createItemResponse(String ISBN) {
         return ItemResponse.create(ISBN);
+    }
+
+    private void assertISBNEquals(String ISBN, ItemResponse response) {
+        assertEquals(ISBN, response.getISBN());
+    }
+
+    private void assertItemsSummarySizeIs(int size, List<ItemSummaryResponse> response) {
+        assertEquals(3, response.size());
     }
 
     private List<ItemSummaryResponse> createSummaryWith3Items() {
